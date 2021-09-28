733f9a8ebfa0110668a93fac5f716fc68bbf3955
GuangLiang Zhou <1176224611@qq.com>
2021-09-28 11:37:00 +0800

    Update SEIRV1.R

--- R/SEIRV1.R
+++ R/SEIRV1.R
@@ -1,7 +1,7 @@
 ###################SEIR#############
      # SEIR: Selector-Embedded Iterative Regression
      # A fast and extremely powerful R package for GWAS
-     # Written by Mengjin Zhu
+     # Written by Mengjin Zhu & GuangLiangZhou
 #############################################################################################################################
      # Object: Main function to perform GWAS
      # Authors: Mengjin Zhu
